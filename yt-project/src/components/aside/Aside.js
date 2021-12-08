ee0ffeb63661a1c790912b41001eeddc3f7bed51
jeanmariemcc <84250957+jeanmariemcc@users.noreply.github.com>
2021-12-08 17:06:11 -0500

    removed console log from Aside

--- yt-project/src/components/aside/Aside.js
+++ yt-project/src/components/aside/Aside.js
@@ -3,7 +3,7 @@ import "./aside.css";
 import { Link } from "react-router-dom";
 
 export default function Aside({ posts }) {
-	console.log(posts);
+	// console.log(posts);
 	return (
 		<div className="aside">
 			<div className="asideItem">

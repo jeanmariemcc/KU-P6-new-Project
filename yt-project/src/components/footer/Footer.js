84402694344eeb62f156bd6c245033ff75e7ae25
Randy McMillen <84250923+RandyMac22@users.noreply.github.com>
2021-12-09 16:28:45 -0600

    Made footer stretch across the bottom and added the header font to it

--- yt-project/src/components/footer/Footer.js
+++ yt-project/src/components/footer/Footer.js
@@ -6,22 +6,20 @@ class Footer extends React.Component {
 	render() {
 		// console.log(links);
 		return (
-			<footer className="header">
+			<footer className="Footer">
 				{/* <footer> */}
-				<div id="parent">
-					<div id="left">
-						<h2>CONTACT US</h2>
-						<p>Request your free evaluation today!</p>
-						<p>
-							<br />
-							We’re here for you when you need us. Call us to
-							schedule an appointment and we’ll get back to you as
-							soon as possible.
-						</p>
-						<p>Boynton Beach, FL 33435</p>
-						<p>info@atlashealthandwellness.com</p>
-						<p>(954) 93-ATLAS</p>
-					</div>
+				<div>
+					<h2>CONTACT US</h2>
+					<p>Request your free evaluation today!</p>
+					<p>
+						<br />
+						We’re here for you when you need us. Call us to
+						schedule an appointment and we’ll get back to you as
+						soon as possible.
+					</p>
+					<p>Boynton Beach, FL 33435</p>
+					<p>info@atlashealthandwellness.com</p>
+					<p>(954) 93-ATLAS</p>
 				</div>
 			</footer>
 		);
